1be7381e3904741b576ee42fbf1187091fb7bc1b
Pie9706 <pietro.montalbano9706@gmail.com>
2023-12-05 15:16:49 +0100

    terzo

--- puntino.js
+++ puntino.js
@@ -1,4 +1,17 @@
-var circle = document.getElementById("circle");
-document.addEventListener("click", function () {
-  console.log("hai cliccato il pulsante!");
+contatoreElement = document.getElementById("contatore");
+incrementaButton = document.getElementById("button1");
+decrementaButton = document.getElementById("button2");
+
+incrementaButton.addEventListener("click", function () {
+  contatoreElement.textContent = +contatoreElement.textContent + 1;
 });
+
+decrementaButton.addEventListener("click", function () {
+  if (contatoreElement.textContent > 0) {
+    contatoreElement.textContent = +contatoreElement.textContent - 1;
+  } else {
+    alert("il valore non può essere inferiore a 0");
+  }
+});
+
+valoreContatore;
